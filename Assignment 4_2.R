6d33c7affac5814cb886fe592665611d2d7f0653
vadde18srinu <40682358+vadde18srinu@users.noreply.github.com>
2018-08-08 15:22:01 +0530

    Update Assignment 4_2.R

--- Assignment 4_2.R
+++ Assignment 4_2.R
@@ -2,13 +2,32 @@
 Perform the below string Operation:
   . Replace the period character "." within each string with another character i.e. "-" minus sign.
 
-x <- c("-data.science.in.R", "-machine.learning.in.R")
-x
+Ans. 
+x <- c("data.science.in.R", "machine.learning.in.R")
+x1=c("data.science.in.R")
+x2=c("machine.learning.in.R")
+x1
+x2
+gsub(pattern ="data.science.in.R", replacement ="data-science-in-R",x1,ignore.case = FALSE)
+gsub(pattern ="machine.learning.in.R", replacement ="machine-learning-in-R",x2,ignore.case = FALSE)
+x1x2=c(gsub(pattern ="data.science.in.R", replacement ="data-science-in-R",x1,ignore.case = FALSE),
+       gsub(pattern ="machine.learning.in.R", replacement ="machine-learning-in-R",x2,ignore.case = FALSE))
+x1x2
 
 
 2. x <- c('data.science.in.R','machine.learning.in.R')
-Perform the below String Operation:
-  . Append again with "-" minus sign character at the start of the each string and finally concatenate all the string within the vector to form a final single string and assigning it the object.
+Perform the below String Operation:Append again with
+"-" minus sign character at the start of the each string and finally 
+concatenate all the string within the vector to form a final single string and assigning it the object.
 
-x <- c("-","data.science.in.R","-","machine.learning.in.R")
-x
+Ans. 
+x <- c("data.science.in.R", "machine.learning.in.R")
+x3=c("data.science.in.R")
+x4=c("machine.learning.in.R")
+x3
+x4
+gsub(pattern ="data.science.in.R", replacement ="-data.science.in.R",x3,ignore.case = FALSE)
+gsub(pattern ="machine.learning.in.R", replacement ="-machine.learning.in.R",x4,ignore.case = FALSE)
+x3x4=c(gsub(pattern ="data.science.in.R", replacement ="-data.science.in.R",x3,ignore.case = FALSE),
+       gsub(pattern ="machine.learning.in.R", replacement ="-machine.learning.in.R",x4,ignore.case = FALSE))
+x3x4
